c9013a1613f4a12ea421aa1b1daec1f6a0e865f3
smokingelephants <bbose007@gmail.com>
2019-04-28 01:48:02 -0500

    Update gan.py

--- gan.py
+++ gan.py
@@ -153,7 +153,7 @@ class GAN():
                 axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/%d.png" % epoch)
+        fig.savefig("sample_data/%d.png" % epoch)
         plt.close()
 
 
